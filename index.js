1c94d635a92bbfcd3dcf5ef64752063c622c41dc
Andrew Durrant <andrew.durrant75@gmail.com>
2020-02-20 15:06:30 -0700

    add part 1 and 2

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+
+function getDogImage(dogNumber) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${dogNumber}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+function tooManyImageResults(dogNumber) {
+  if(dogNumber > 50) {
+    throw new Error('Number is too big. Try again');
+  }
+  if(dogNumber < 1) {
+    throw new Error('Number is too small. Try again');
+  }
+}
+
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
+  let imageStr = '';
+  responseJson.message.forEach(image => {
+    imageStr += `<img src="${image}"><br>`;
+  })
+  
+  $('.display').html(imageStr);
+
+
+
+  // $('.results-img').replaceWith(
+  //   `<img src="${responseJson.message}" class="results-img">`
+  // )
+
+
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -21,7 +42,15 @@ function displayResults(responseJson) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+
+    const dogNumber = $('#dogs').val();
+    try {
+    tooManyImageResults(dogNumber);
+    getDogImage(dogNumber);
+    }
+    catch(error) {
+      alert(error.message);
+    }
   });
 }
 
